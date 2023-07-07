3d0c93ff23200ccb9380389ddc6a2a1a39e16c33
Alena <alenabelovadmvn@gmail.com>
2023-07-07 12:54:58 -0400

    Initial Commit

---
+++ Vision App DEMO_1/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Vision App DEMO_1
+//
+//  Created by Alena Belova  on 2023-07-07.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vision App DEMO_1/Vision_App_DEMO_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_App_DEMO_1App.swift
+//  Vision App DEMO_1
+//
+//  Created by Alena Belova  on 2023-07-07.
+//
+
+import SwiftUI
+
+@main
+struct Vision_App_DEMO_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
